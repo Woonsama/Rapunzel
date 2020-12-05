a536845e08fa969075e6d3bf891d3ac5b8962115
Woonsama <kws4510@naver.com>
2020-12-05 21:36:23 +0900

    Add CameraShake

---
+++ RapunzelRoot/Assets/02.Scripts/Helper/ExtensionMethod.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExtensionMethod : MonoBehaviour
+{
+    public static bool isCanShake = true;
+
+    public static IEnumerator Shake(Transform targetTransform, float magnitude, float duration)
+    {
+        if(isCanShake)
+        {
+            isCanShake = false;
+            Vector3 originalPos = targetTransform.position;
+            float time = 0;
+
+            while (time <= duration)
+            {
+                time += Time.deltaTime;
+                float x = Random.Range(-1.0f, 1.0f) * magnitude;
+                float y = Random.Range(-1.0f, 1.0f) * magnitude;
+
+                targetTransform.position = Vector3.MoveTowards(targetTransform.position, originalPos + new Vector3(x, y, 0), 1);
+                yield return null;
+            }
+
+            targetTransform.position = originalPos;
+            isCanShake = true;
+        }
+    }
+}
---
+++ RapunzelRoot/Assets/02.Scripts/Logic/KWS/Logic/CameraManager.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraManager : MonoBehaviour
+{
+    public void Do_Shake(float magnitude, float duration)
+    {
+        StartCoroutine(ExtensionMethod.Shake(Camera.main.transform, magnitude, duration));
+    }
+}
