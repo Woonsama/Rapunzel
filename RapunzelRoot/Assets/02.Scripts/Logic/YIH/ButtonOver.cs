f63ad1d4d5015033070086a9c554422244887740
frozenHamster <dlsgkr2856@naver.com>
2020-12-06 02:46:21 +0900

    MainUIAdd

---
+++ RapunzelRoot/Assets/02.Scripts/Logic/YIH/ButtonOver.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonOver : MonoBehaviour
+{
+    [SerializeField]
+    [Header("롤오버이미지")]
+    Sprite loloverimage;
+
+    Image thisimage;
+
+    Sprite beforeImage;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        thisimage = GetComponent<Image>();
+
+        beforeImage = thisimage.sprite;
+    }
+
+	private void OnMouseEnter()
+	{
+        thisimage.sprite = loloverimage;
+
+    }
+	private void OnMouseExit()
+	{
+        thisimage.sprite = beforeImage;
+
+    }
+}
