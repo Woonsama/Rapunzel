e3e5ae02f55d7fb8bbd7fb795d5271c997d8cc39
Woonsama <kws4510@naver.com>
2020-12-05 15:29:41 +0900

    골드, 스코어 기능 추가

--- RapunzelRoot/Assets/02.Scripts/Data/DataManager.cs
+++ RapunzelRoot/Assets/02.Scripts/Data/DataManager.cs
@@ -4,8 +4,12 @@ using UnityEngine;
 
 public class DataManager : SingletonMonoBase<DataManager>
 {
+    GameData gameData;
+
     private void Awake()
     {
         DontDestroyOnLoad(this);
+
+        gameData = new GameData();
     }
 }
---
+++ RapunzelRoot/Assets/02.Scripts/Data/SaveData.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameData
+{
+
+    public int iGold { get; private set; }
+    public int iScore { get; private set; }
+
+    public GameData()
+    {
+        iGold = 0;
+        iScore = 0;
+    }
+
+    public void Do_Add_Or_Minus_Gold(int gold)
+    {
+        iGold += gold;
+        SaveData();
+    }
+
+    public void Do_Add_Score(int score)
+    {
+        iScore += score;
+        SaveData();
+    }
+
+    public void SaveData()
+    {
+        PlayerPrefs.SetInt("Gold", iGold);
+        PlayerPrefs.SetInt("Score", iGold);
+    }
+
+    public void LoadData()
+    {
+        iGold = PlayerPrefs.GetInt("Gold");
+        iScore = PlayerPrefs.GetInt("Score");
+    }
+
+}
