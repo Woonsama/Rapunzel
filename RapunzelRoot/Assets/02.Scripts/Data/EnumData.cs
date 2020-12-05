f1f7eb7ad684c8aeedfcf82e41af26280ee44f45
Woonsama <kws4510@naver.com>
2020-12-05 21:43:51 +0900

    Add Enum Data

---
+++ RapunzelRoot/Assets/02.Scripts/Data/EnumData.cs
@@ -0,0 +1,21 @@
+﻿
+
+namespace EnumData.ColorType
+{
+    public enum EEnemyColorType
+    {
+        Red = 0,
+        Blue,
+        Green,
+        Yellow,
+    }
+
+    public enum  EPotionColorType
+    {
+        Red = 0,
+        Blue,
+        Green,
+        Yellow,
+        Gold,
+    }
+}
